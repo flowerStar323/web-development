e3c7b8f9619eefa67bb12552d46cffd729ebf998
flowerStar323 <wstar3233@gmail.com>
2021-02-16 17:32:56 -0500

    Lazy Push

--- 2-JavaScript/Project_Spy/spy.js
+++ 2-JavaScript/Project_Spy/spy.js
@@ -1,7 +1,7 @@
 // var prenom = prompt("Welcome to the site !\n Entrer your name :")
 // var nom = prompt("Enter surname :")
 // var age = prompt("Enter age : ")
-// var height = prompt("Enter height in cm :")
+ var height = prompt("Enter height in cm :")
 // var alias = prompt("Enter alias :")
 
 // if ((prenom[0]==nom[0]) && (age>20 && age<30) &&  (alias.slice(-1)=="y"))
@@ -11,3 +11,4 @@
 // else{
 //     console.log("R.A.S")
 // }
+
