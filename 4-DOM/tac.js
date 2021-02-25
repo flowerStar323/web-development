ed3c3e710520c2d79b986b58a098ffa8d43176ad
flowerStar323 <wstar3233@gmail.com>
2021-02-25 14:05:09 -0500

    Lazy Push

--- 4-DOM/tac.js
+++ 4-DOM/tac.js
@@ -5,7 +5,7 @@ var cases = document.querySelectorAll("td");
 
 function clearBoard() 
 {
-    for(let i= 0 ; i <=cases.length;i++)
+    for(let i= 0 ; i <cases.length;i++)
     cases[i].textContent= ''
 }
 restart.addEventListener("click",clearBoard);
