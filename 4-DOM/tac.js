8d70cd982ed2a7544ed9eb3e23764be298ed2988
flowerStar323 <wstar3233@gmail.com>
2021-02-25 14:02:45 -0500

    Lazy Push

--- 4-DOM/tac.js
+++ 4-DOM/tac.js
@@ -1,7 +1,7 @@
 
 //console.log("Connected");
 var restart = document.querySelector("#b");
-var cases = document.querySelectorAll(td);
+var cases = document.querySelectorAll("td");
 
 function clearBoard() 
 {
