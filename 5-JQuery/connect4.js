6bedd79c3bd9cb7d14d109b3dcaa275d39d65296
flowerStar323 <wstar3233@gmail.com>
2021-03-04 08:38:11 -0500

    Lazy Push

--- 5-JQuery/connect4.js
+++ 5-JQuery/connect4.js
@@ -1,2 +1,2 @@
-var player_1=prompt("Enter the name for player 1 (Red)")
-var player_2=prompt("Enter the name for player 2 (Blue)")
+var player_1=prompt("Enter the name for player 1 (Blue)")
+var player_2=prompt("Enter the name for player 2 (Red)")
