5e209ecb4912d71fea4a507dd06184fe556a46fc
flowerStar323 <wstar3233@gmail.com>
2021-03-07 18:10:40 -0500

    Lazy Push

--- 6-Python/Part6_Exercise_Review.py
+++ 6-Python/Part6_Exercise_Review.py
@@ -70,3 +70,4 @@ name = "Sammy"
 
 # Use print formatting to print the following string:
 "Hello my dog's name is Sammy and he is 4 years old"
+print("Hello my dog's name is {} and he is {} years old".format{age,name})
