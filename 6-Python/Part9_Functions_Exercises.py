dea5e6d5be4635806d5c3c6fa80c869d627952c3
flowerStar323 <wstar3233@gmail.com>
2021-03-07 18:51:23 -0500

    Lazy Push

--- 6-Python/Part9_Functions_Exercises.py
+++ 6-Python/Part9_Functions_Exercises.py
@@ -108,7 +108,7 @@ def no_teen_sum(a, b, c):
   return fix_teen(a)+fix_teen(b)+fix_teen(c)
 def fix_teen(n):
   # CODE GOES HERE
-  if (n>13 and n<19 and n!=15 and n!=16):
+  if n in [13,14,17,18,19]:
     return n
   else:
     return 0
