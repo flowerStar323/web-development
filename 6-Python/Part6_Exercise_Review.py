cd9cb3a7c3d37e765bc04753aedf418fc4299891
flowerStar323 <wstar3233@gmail.com>
2021-03-07 18:03:52 -0500

    Lazy Push

--- 6-Python/Part6_Exercise_Review.py
+++ 6-Python/Part6_Exercise_Review.py
@@ -44,7 +44,7 @@ print(l)
 # Using keys and indexing, grab the 'hello' from the following dictionaries:
 
 d1 = {'simple_key':'hello'}
-
+print(d1[simple_key])
 d2 = {'k1':{'k2':'hello'}}
 
 d3 = {'k1':[{'nest_key':['this is deep',['hello']]}]}
